e8d6580c30f53f7aa338e9c86d13da10c21a2050
rororyo <ryonathaniel05@gmail.com>
2024-10-28 21:36:16 +0700

    feat: modified readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SocketProgrammingDES
+
+| Name | NRP | Kelas |
+| --- | --- | ----------|
+| Nathaniel Ryo Kurniadi | 5025221019 | Keamanan Informasi A|
+| Steven Gerard  |  | Keamanan Informasi A|
+
+
+# Pembagian Tugas
+Steven Gerard           : Modifikasi algoritma DES agar dapat menerima string dengan panjang lebih dari 64 bit dan mencoba socket programming
+Nathaniel Ryo Kurniadi  : Menggunakan SocketIO untuk socket programming, menyempurnakan algoritma DES yang telah diimplementasi, menulis readme
--- server.py
+++ server.py
@@ -1,31 +1,26 @@
-#imports 
+
 import socketio
 import eventlet
 from eventlet import wsgi
 
-# Create a Socket.IO server
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
-# Handle new connections
 @sio.event
 def connect(sid, environ):
     print(f'User connected: {sid}')
     sio.emit('message', {'msg': f'User {sid} has entered the chat!'}, to=sid)
 
-# Handle messages from clients
 @sio.event
 def message(sid, data):
     print(f'Received message from {sid}: {data["msg"]}')
     sio.emit('message', {'msg': f'{sid}: {data["msg"]}'}, skip_sid=sid)
 
-# Handle client disconnections
 @sio.event
 def disconnect(sid):
     print(f'User disconnected: {sid}')
     sio.emit('message', {'msg': f'User {sid} has left the chat.'})
 
-# Run the server
 if __name__ == '__main__':
     print("Chat server started on http://localhost:5000")
     wsgi.server(eventlet.listen(('', 5000)), app)
